a410d834b2eb7a025d7af3fb32d1ac7bf47d55b2
Fudai1020 <fuudai1020@gmail.com>
2025-08-03 19:41:05 +0900

    add:hellojava

---
+++ backend/kakeibo-app-java/src/main/java/kakeibo_app_java/kakeibo_app_java/KakeiboAppJavaApplication.java
@@ -0,0 +1,13 @@
+package kakeibo_app_java.kakeibo_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboAppJavaApplication.class, args);
+	}
+
+}
---
+++ backend/kakeibo-app-java/src/test/java/kakeibo_app_java/kakeibo_app_java/KakeiboAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package kakeibo_app_java.kakeibo_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeiboAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
